0afebb0bd1b5ae62983173dc68b856624b714cb9
Artur Rog <artur.rog95@gmail.com>
2016-12-27 21:53:41 +0100

    Drobne poprawki

--- posts/admin.py
+++ posts/admin.py
@@ -5,8 +5,20 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Post, Comment, Report
+from category.models import Category
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Report)
 
+# Ewentualnie zeby byly tylko te posty, do ktorych mamy dostep
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     def queryset(self, request):
+#         qs = super(admin.ModelAdmin, self).queryset(request)
+#
+#         if request.user.is_superuser:
+#             return qs
+#
+#         user_qs = Post.objects.filter(category=request.user.id)
+#         return qs.filter(author__in=user_qs)
