f423c5f5d5e5b1a5d709148a21c6d6598afbf1ad
Artur Rog <artur.rog95@gmail.com>
2017-01-10 00:41:22 +0100

    Heroku

--- SocialNewsService/settings.py
+++ SocialNewsService/settings.py
@@ -141,9 +141,14 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, '../static'),
+)
 
 LOGIN_REDIRECT_URL = 'home'
 
